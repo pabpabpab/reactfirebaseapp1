21428f2b95983dd8eb82d9cca27d8bf289f86d2f
pabpabpab <urallove@yandex.ru>
2022-10-15 10:05:58 +0500

    firebase start

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.sass';
+import theme from './theme/theme';
+import { ThemeProvider } from '@mui/material/styles';
+import {Route, Routes} from 'react-router-dom';
+import {Fragment} from "react";
+
+import HomePage from './pages/HomePage';
+import RegisterPage from './pages/RegisterPage';
+import LoginPage from './pages/LoginPage';
+import MessengerPage from './pages/MessengerPage';
+import ProfilePage from './pages/ProfilePage';
+import NotFoundPage from './pages/NotFoundPage';
+
+import PrivateRoute from './components/PrivateRoute';
+import PublicRoute from './components/PublicRoute';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    return (
+            <ThemeProvider theme={theme}>
+                <Routes>
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="/register" element={<PublicRoute><RegisterPage /></PublicRoute>} />
+                    <Route path="/login" element={<PublicRoute><LoginPage /></PublicRoute>} />
+                    <Route path="/messenger/*" element={<MessengerPage />} />
+                    <Route path="/profile/:userId" element={<PrivateRoute><ProfilePage /></PrivateRoute>} />
+                    <Route path="*" element={<NotFoundPage />} />
+                </Routes>
+            </ThemeProvider>
+    );
 }
 
 export default App;
+/*
+<Route path="/messenger/*" element={<PrivateRoute><MessengerPage /></PrivateRoute>} />
+ */
